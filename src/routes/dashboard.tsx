a05e16134563d6fcf3c0ccc1050487be4645b080
Ajibola Gbenga Joseph <ajibolagbengajoseph@gmail.com>
2026-04-27 02:38:29 +0100

    Fix build error: remove broken SVG fragment causing unclosed div in dashboard.tsx

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -836,16 +836,6 @@ function DashboardLayout() {
             </div>
           </div>
         </div>
-      )}" strokeLinecap="round" strokeLinejoin="round">
-                      <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/><path d="M15.54 8.46a5 5 0 0 1 0 7.07"/><path d="M19.07 4.93a10 10 0 0 1 0 14.14"/>
-                    </svg>
-                  </button>
-                  <span className="text-white/60 text-[11px]">Speaker</span>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
       )}
     </div>
   );
